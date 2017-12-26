aab84df4962a4183ea9ea0b85c215033816b1925
Palente <Palente@users.noreply.github.com>
2017-12-26 19:31:57 +0100

    Added the param function
    Now you can change the cost of the heal by doing /hl param 2000 to set in to 2000

--- src/Palente/Healther/Main.php
+++ src/Palente/Healther/Main.php
@@ -3,11 +3,11 @@ namespace Palente\Healther;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as TX;
 use pocketmine\level\Level;
 use pocketmine\utils\Config;
 use pocketmine\command\Command;
-use pocketmine\command\CommandSender;;
+use pocketmine\command\CommandSender;
 use pocketmine\event\TranslationContainer;
 class Main extends PluginBase{
 	public static $logger = null;
@@ -31,11 +31,11 @@ if(!$eco){
             $this->setEnabled(false);
         }
 @mkdir($this->getDataFolder());
-f(!file_exists($this->getDataFolder() . "config.yml")) {
+if(!file_exists($this->getDataFolder() . "config.yml")) {
 	$this->saveResource("config.yml");
 	self::$logger->notice("[Healther]::[Folder] The file config.yml was created Successfully");
-}else{
 }
+	}
 /*
 *Fonction qui vérifie si le joueur a assez puis remet sa vie
 *
@@ -49,4 +49,32 @@ if($eco->myMoney($player->getName()) < $config->get("cost")) return false;
 $eco->reduceMoney($player->getName(), $config->get("cost"));
 $player->setHealth(20);
 }
+public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+	$config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+if($cmd->getName() == "hl"){
+	switch(count($args)){
+		case 0:
+			if(!$sender instanceof Player) return false;
+			if(!$sender->hasPermission("healther.use")) return $sender->sendMessage($pr.TX::RED."Error: You can't use that command");
+			if($sender->getLevel()->getName() == $config->get("restrictedlevel")){
+				$sender->sendMessage($pr.TX::RED."ERROR: You can't regen your health with that in this world!");
+				return false;
+			}
+		$this->healPlayer($this->getServer()->getPlayer($sender));
+		break;
+		case 2:
+			if($args[0] == "param"){
+				if(!$player->isOp()) return false;
+			$mont = int($args[1]);
+				$config->set("cost",$mont);
+				$config->save();
+				$config->reload();
+				$sender->sendMessage($pr."The new cost of the /hl is now set to".$cost);
+			}else{
+				return false;}
+		break;
+	}
 }
+}
+}
+	
