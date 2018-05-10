40e9c3d09ac2985b73d9fa24fa6527c8d2029588
Palente(Adel) <Palente@users.noreply.github.com>
2018-05-11 00:33:46 +0200

    updated main

--- src/Palente/Healther/Main.php
+++ src/Palente/Healther/Main.php
@@ -16,25 +16,18 @@ class Main extends PluginBase{
 	/**
 	*s'execute au chargement du plugin
 	*/
-	public function onLoad(){
-		self::$logger = $this->getLogger();
-		self::$logger->info("[Healther]Build: BETA!!!");
-		self::$logger->info("[Healther]By Palente");
-		self::$logger->info("[Healther]twitter: @Palente_Gaming");
-		self::$logger->info("[Healther]Version: 1");
-}
+	
 	public function onEnable(){
-self::$logger->info("[Healther] Enabled");
 $eco = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
 if(!$eco){
-    $this->getLogger()->critical("ERROR: EconomyAPI???.");
+    $this->getLogger()->critical("ERROR: I couldn't Find EconomyAPI!!Error :(");
             #$this->setEnabled(false);
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
 @mkdir($this->getDataFolder());
 if(!file_exists($this->getDataFolder() . "config.yml")) {
 	$this->saveResource("config.yml");
-	self::$logger->notice("[Healther]::[Folder] The file config.yml was created Successfully");
+	self::$logger->notice("[Healther]::[Folder] I couldn't find a config....So i created it!");
 }
 	}
 /*
@@ -56,26 +49,36 @@ if($cmd->getName() == "hl"){
 	switch(count($args)){
 		case 0:
 			if(!$sender instanceof Player) return false;
-			if(!$sender->hasPermission("healther.use")) return $sender->sendMessage($pr.TX::RED."Error: You can't use that command");
+			if(!$sender->hasPermission("healther.use")) return $sender->sendMessage($pr.TX::RED."Error: You don't have the permission to use the command /heal");
 			if($sender->getLevel()->getName() == $config->get("restrictedlevel")){
-				$sender->sendMessage($pr.TX::RED."ERROR: You can't regen your health with that in this world!");
+				$sender->sendMessage($pr.TX::RED."ERROR: You can't regen your health in this world!");
 				return false;
 			}
 		$this->healPlayer($this->getServer()->getPlayer($sender));
 		break;
 		case 2:
-			if($args[0] == "param"){
+			if($args[0] == "money"){
 				if(!$player->isOp()) return false;
 			$mont = int($args[1]);
 				$config->set("cost",$mont);
 				$config->save();
 				$config->reload();
-				$sender->sendMessage($pr."The new cost of the /hl is now set to".$cost);
-			}else{
-				return false;}
+				$sender->sendMessage($pr."You have setted the new cost of The /heal to".$cost);
+			}elseif($args[0] == "level"){
+			if(!$player->isOp()){ $sender->sendMessage($pr."You don't have the permission to use that command");
+					     return false;}
+				$leveln = $args[1];
+				$level = $this->getServer()->getLevelByName($leveln);
+				if(!$level instanceof Level){$sender->sendMessage($pr."Error: The level ".$leveln." seem to not exit");
+							     return false;}
+			$config->set("restrictedlevel",$level->getName());
+				$config->save();
+				$config->reload();
+				$sender->sendMessage($pr.TX::YELLOW."You have successfully set the new restricted level to ".$leveln);
+			
+				return;}else{return;}
 		break;
 	}
 }
 }
 }
-	
